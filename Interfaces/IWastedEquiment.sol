d2563546afb0c4b7fb3c07bd3fd2d1d96da5fe14
Night mare <hnbtranspkt@gmail.com>
2021-10-01 21:26:52 +0700

    [Update] Nightmare: Update new interface of equipment and draft wasted hero smartcontract

--- Interfaces/IWastedEquiment.sol
+++ Interfaces/IWastedEquiment.sol
@@ -4,23 +4,22 @@ pragma solidity ^0.8.0;
 
 interface IWastedEquipment {
     // Event required.
-    event ItemCreated(uint indexed itemId, string name, uint16 maxSupply, ItemType itemType, Rarity rarity);
-    event ItemUpgradable(uint indexed itemId, uint indexed nextTierItemId, uint8 upgradeAmount);
-    event UpgradedItem()
-    
     enum ItemType { WEAPON, ARMOR, ACCESSORY, SKILL } // 4 types of equipments.
     enum Rarity { COMMON, UNCOMMON, RARE, EPIC, LEGENDARY }
+    event ItemCreated(uint indexed itemId, string name, uint16 maxSupply, ItemType itemType, Rarity rarity);
+    event ItemUpgradable(uint indexed itemId, uint indexed nextTierItemId, uint8 upgradeAmount);
+    event ItemUpdated(uint itemId);
     
     
     struct WastedItem {
         ItemType itemType;
         Rarity rarity;
         string name;
-        uint16 maxSupply; // max total supply of this item.
-        uint16 minted; // total item minted.
-        uint16 burnt; //total burnt to upgrade item
-        uint8 star; // star of item
-        uint8 requireAmount; //require amount item to upgrade to next star. 
+        uint16 maxSupply;
+        uint16 minted;
+        uint16 burnt;
+        uint8 star;
+        uint8 requireAmountToUpgrade;
     }
     
      /**
@@ -32,6 +31,11 @@ interface IWastedEquipment {
     function rollWastedEquipment(uint vendorId, uint amount) external;
     
     /**
+     * @notice Burns ERC1155 equipment since it is equipped to the knight.
+     */
+    function putItemsIntoStorage(address account, uint[] memory itemIds) external;
+    
+    /**
      * @notice Check if item is out of stock.
      */
     function isOutOfStock(uint itemId, uint16 amount) external view returns (bool);
@@ -40,6 +44,11 @@ interface IWastedEquipment {
      * @notice Create an wasted item.
      */
     function createWastedItem(string memory name, uint16 maxSupply, ItemType itemType, Rarity rarity) external;
+    
+    /**
+     * @notice Returns ERC1155 equipment back to the owner.
+     */
+    function returnItems(address account, uint[] memory itemIds) external;
 
     /**
      * @notice Add next star of item to existing one.
@@ -63,7 +72,7 @@ interface IWastedEquipment {
     * @notice Get informations of item by itemId
     */
     
-     function getWastedItem(uint itemId) external view returns (Item memory item);
+     function getWastedItem(uint itemId) external view returns (WastedItem memory item);
 
     /**
      * @notice Gets wasted item type.
